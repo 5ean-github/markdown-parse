5aee4b50b1fdedaf1e3b2eeb9544bfee8a3b8a64
seanchen-ai <61416948+seanchen-ai@users.noreply.github.com>
2022-02-03 16:18:09 -0800

    fixed test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,19 +8,10 @@ import java.util.*;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(2, 1);
-    }
     public void parseTestOne() throws IOException{
         Path fileName = Path.of("test-file.md");
         String contents = Files.readString(fileName);
-        List<String> test1 = List.of("https://something.com","some-page.html");
+        List<String> test1 = List.of("a link on the first line");
         assertEquals("test 1", test1, MarkdownParse.getLinks(contents));
-    }
-    public void parseTestTwo() throws IOException{
-        Path fileName = Path.of("test-file2.md");
-        String contents = Files.readString(fileName);
-        List<String> test2 = List.of("https://something.com","some-page.html");
-        assertEquals("test 2", test2, MarkdownParse.getLinks(contents));
     }
 }
